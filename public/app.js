1e229e42da4977eed9c3b885ea772e444464751b
Just-Martin-Really <martin.liess@digitalgedacht.de>
2026-02-07 22:48:51 +0100

    redirect to forum page upon successful registration

--- public/app.js
+++ public/app.js
@@ -111,6 +111,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 usernameField.value = '';
                 passwordField.value = '';
                 courseField.value = '';
+                window.location.href = "Forumpage.html";
             }
         });
     }
