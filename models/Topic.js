f48443c6d7f053f73b1cb9383dd74a316c15d102
Just-Martin-Really <martin.liess@digitalgedacht.de>
2026-02-08 14:28:47 +0100

    added kurs to topics for filter and search

--- models/Topic.js
+++ models/Topic.js
@@ -10,6 +10,7 @@ const mongoose = require('mongoose');
 const topicSchema = new mongoose.Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
+    kurs: { type: String, required: true, enum: ['TIA', 'TIS', 'TIK'] },
     createdAt: { type: Date, default: Date.now }
 });
 
